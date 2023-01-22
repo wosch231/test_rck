9ee1de50277156521b4ca8e3f189134438e62e83
wosch231 <77884030+wosch231@users.noreply.github.com>
2023-01-22 20:03:35 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,9 @@
+namespace tt {
 let ix = 0
 let iy = 0
-input.onButtonPressed(Button.A, function () {
-    tt.ftest(1.5, 2.4)
-    basic.showNumber(ix)
-    basic.showNumber(iy)
-})
-namespace tt {
     //% blockId=tt_ftest
-    //% block="Setze den Zeichnungsparamerter nrpar %nrpar auf wert %wert"
+    //% block="Test function %x0 und %y0"
     //% inlineInputMode=inline
-    //% nrpar.min=0 nrpar.max=6
-    //% wert.min=0 wert.max=6
     //% weight=100 blockGap=8
     export function ftest(x0: number, y0: number) {
         ix = Math.round(x0 + 1.1)
@@ -18,7 +11,3 @@ namespace tt {
         return
     }
 }
-
-basic.forever(function () {
-	
-})
